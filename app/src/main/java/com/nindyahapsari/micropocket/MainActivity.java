49b3699baa63716449a274aae6f306ca8adc5289
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-03-09 02:05:14 +0100

    update UI, added login

--- app/src/main/java/com/nindyahapsari/micropocket/MainActivity.java
+++ app/src/main/java/com/nindyahapsari/micropocket/MainActivity.java
@@ -1,13 +1,37 @@
 package com.nindyahapsari.micropocket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        loginButton = (Button) findViewById(R.id.btn_login);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
+
+
